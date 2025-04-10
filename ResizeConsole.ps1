d1d51aa3bb992d73bdb17ab32075e0c6995c9af4
Lasne Olivier <4268373+0xfalafel@users.noreply.github.com>
2025-04-10 12:03:38 +0200

    Fix issue when updating Buffersize

--- ResizeConsole.ps1
+++ ResizeConsole.ps1
@@ -1,4 +1,4 @@
 $width=80
 $height=24
-$Host.UI.RawUI.BufferSize = New-Object Management.Automation.Host.Size ($width, $height)
-$host.UI.RawUI.WindowSize = New-Object -TypeName System.Management.Automation.Host.Size -ArgumentList ($width, $height)
+$Host.UI.RawUI.BufferSize = New-Object Management.Automation.Host.Size (1000, 9999)
+$host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size ($width, $height)
